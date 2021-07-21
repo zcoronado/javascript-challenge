3aec28a6811e87549647052dc9a4c0be5a8d5489
Zaira Coronado <coronado.zaira@gmail.com>
2021-07-20 20:01:53 -0700

    Level 1 and Level 2 complete

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,33 @@
 // from data.js
-var tableData = data;
+// var tableData = data;
 
-// YOUR CODE HERE!
+showData(data);
+
+// Select the button 
+var button = d3.select("#filter-btn"); 
+
+// Create event handlers 
+button.on("click", runEnter);
+
+// Complete the event handler function for the form
+function runEnter() {
+
+// Prevent the page from refreshing
+d3.event.preventDefault();
+
+// Select the input element and get the raw HTML node
+var inputElement = d3.select("#datetime").node().value;
+var tableData = data.filter(obj=>obj.datetime==inputElement);
+
+showData(tableData);
+}
+
+function showData(data) {
+    d3.select('tbody').html('')
+    data.forEach(obj => {
+        var row = d3.select('tbody').append('tr');
+        Object.values(obj).forEach(val=>{
+            row.append('td').text(val)
+        });
+    });
+}
--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -1,4 +1,40 @@
 // from data.js
-var tableData = data;
+var filteredData = data;
 
-// YOUR CODE HERE!
+showData(data);
+d3.selectAll('input').on('change',handleChange);
+
+// Select the button 
+var button = d3.select("button"); 
+
+// Create event handlers 
+button.on("click", runEnter);
+
+// Complete the event handler function for the form
+function runEnter() {
+
+// Prevent the page from refreshing
+// d3.event.preventDefault();
+
+// Select the input element and get the raw HTML node
+d3.selectAll('input').property('value','');
+showData(data);
+}
+
+function showData(data) {
+    d3.select('tbody').html('')
+    data.forEach(obj => {
+        var row = d3.select('tbody').append('tr');
+        Object.values(obj).forEach(val=>{
+            row.append('td').text(val)
+        });
+    });
+};
+
+function handleChange() {
+    var key = d3.select(this).property('id');
+    var value = d3.select(this).property('value');
+
+    filteredData = filteredData.filter(obj=> obj[key] == value);
+    showData(filteredData);
+}
